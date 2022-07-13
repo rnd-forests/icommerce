0cc48b601a7b66db338b62f57c5367b412243a8a
Vinh Nguyen <vinh@inspectorio.com>
2022-07-13 10:10:39 +0700

    cleanup local server logs

--- packages/order-processor-service/webpack.server.js
+++ packages/order-processor-service/webpack.server.js
@@ -1,7 +1,8 @@
-const config = require('@lib/webpack/webpack.server.js')({
+const { createLogger } = require('@lib/webpack/util');
+
+module.exports = require('@lib/webpack/webpack.server.js')({
   hmr: true,
   typecheck: true,
   entry: './src/index.ts',
+  logger: createLogger(),
 });
-
-module.exports = config;
